c9165164986795df4c362b82f883493e9e3fe729
hardworking-toptal-dev <161673729+hardworking-toptal-dev@users.noreply.github.com>
2023-06-28 10:25:46 +0500

    Added: initialize function and empty fn code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,51 @@
-use soroban_sdk::{contractimpl, Env};
+#![no_std]
+use soroban_sdk::{contractimpl, contracttype, Env, Address, Symbol};
+
+
+const INIT: Symbol = Symbol::short("initialized");
+
+#[contracttype]
+#[derive(Clone)]
+pub enum DataKey {
+    RewardToken,
+    TokenAdmin,
+}
 
 pub struct StakingContract;
 
 #[contractimpl]
 impl StakingContract {
-    pub fn new() -> StakingContract {
-        StakingContract{}
+
+    pub fn initialize(
+        env: Env,
+        reward_token: Address,
+        token_admin: Address,
+    ) {
+        token_admin.require_auth();
+        assert!(
+            !env.storage().has(&DataKey::TokenAdmin),
+            "already initialized"
+        );
+
+        env.storage().set(&DataKey::RewardToken, &reward_token);
+        env.storage().set(&DataKey::TokenAdmin, &token_admin);
+
+        env.events().publish((INIT, Symbol::short("staking")), token_admin);
+    }
+
+    pub fn stake(env: Env) {
+
+    }
+
+    pub fn unstake(env: Env) {
+
+    }
+
+    pub fn claim_reward(env: Env) {
+
+    }
+
+    pub fn withdraw(env: Env) {
+
     }
 }
